a853ff40039d34974d6a383c4a2021872ddf4afb
StevePro7 <steveprozune@hotmail.com>
2021-06-25 16:08:09 +0100

    no message

--- ch5/findlinks3/main.go
+++ ch5/findlinks3/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"learning/links"
+)
+
+// breadthFirst calls f for each item in the worklist.
+// Any items returned by f are added to the worklist.
+// f is called at most once for each item.
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+// crawl
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return list
+}
 
 func main() {
-	fmt.Println("Hello there")
+	// Crawl the web breadth-first
+	// starting from the command line arguments
+	//breadthFirst(crawl, os.Args[1:])
+
+	var worklist []string
+	worklist = append(worklist, "http://www.google.com")
+	breadthFirst(crawl, worklist)
 }
