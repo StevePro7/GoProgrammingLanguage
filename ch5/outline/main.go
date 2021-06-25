3dfd8d6142c65789a5b39f713a8b7946cde3f712
StevePro7 <steveprozune@hotmail.com>
2021-06-25 15:33:53 +0100

    no message

--- ch5/outline/main.go
+++ ch5/outline/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
 
 func main() {
-	fmt.Println("Hello there")
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+	outline(nil, doc)
+}
+
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data) // push tag
+		fmt.Println(stack)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
 }
