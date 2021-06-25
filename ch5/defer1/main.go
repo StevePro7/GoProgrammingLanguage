3f72b1fe5e3f9cd225cefede0dce0cb6bee5f986
StevePro7 <steveprozune@hotmail.com>
2021-06-25 15:08:38 +0100

    no message

--- ch5/defer1/main.go
+++ ch5/defer1/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func f(x int) {
+	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
+}
 func main() {
-	fmt.Println("Hello there")
+	f(3)
 }
--- ch5/defer2/main.go
+++ ch5/defer2/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func printStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	os.Stdout.Write(buf[:n])
+}
+func f(x int) {
+	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
+}
 
 func main() {
-	fmt.Println("Hello there")
+	defer printStack()
+	f(3)
 }
