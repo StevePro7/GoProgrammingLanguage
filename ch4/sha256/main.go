fc813c3d9b3b55dd350fce46a5efa6c2e0bf32a0
StevePro7 <steveprozune@hotmail.com>
2021-06-25 12:53:03 +0100

    no message

--- ch4/sha256/main.go
+++ ch4/sha256/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello there")
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
 }
