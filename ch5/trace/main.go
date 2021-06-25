9ccd7371c8ea58cf32c5b2136999c51fe01d3742
StevePro7 <steveprozune@hotmail.com>
2021-06-25 16:46:23 +0100

    no message

--- ch5/trace/main.go
+++ ch5/trace/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func bigSlowOPeration() {
+	defer trace("bigSlowOPeration")() // don't forget the extra parentheses
+	// ...lots of work...
+	time.Sleep(10 * time.Second) // simulate slow operation by sleeping
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit  %s (%s)", msg, time.Since(start))
+	}
+}
 
 func main() {
-	fmt.Println("Hello there")
+	fmt.Println("Start")
+	bigSlowOPeration()
+	fmt.Println("-END-")
 }
