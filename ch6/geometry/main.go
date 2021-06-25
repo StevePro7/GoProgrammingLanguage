75cbd76fc612c862af3c148713e83fb2da75ba96
StevePro7 <steveprozune@hotmail.com>
2021-06-25 17:43:46 +0100

    no message

--- ch6/geometry/main.go
+++ ch6/geometry/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import "math"
+
+type Point struct {
+	X, Y float64
+}
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// same thing but as a method of the Point type
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// a path is a journey connecting the points with straight lines
+type Path []Point
+
+// distance returns the distance travelled along the path
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+
+	return sum
+}
 
 func main() {
-	fmt.Println("Hello there")
+
 }
