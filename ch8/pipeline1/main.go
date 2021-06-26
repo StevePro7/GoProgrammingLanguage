95295f65fefba526d37866dd948ad818485c1bc2
StevePro7 <steveprozune@hotmail.com>
2021-06-26 12:40:32 +0100

    no message

--- ch8/pipeline1/main.go
+++ ch8/pipeline1/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello there")
+
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Counter
+	go func() {
+		for x := 0; ; x++ {
+			log.Printf("C x = %d  ", x)
+			naturals <- x
+			log.Printf("C N = %d  ", x)
+			log.Println()
+		}
+	}()
+
+	// Squares
+	go func() {
+		for {
+			log.Printf("S N1= %d  ", naturals)
+			log.Printf("S S1= %d  ", squares)
+			x := <-naturals
+			squares <- x * x
+			log.Printf("S N2= %d  ", naturals)
+			log.Printf("S S2= %d  ", squares)
+		}
+	}()
+
+	// Printer (in main goroutines)
+	log.Print("MAIN squares")
+	fmt.Println(<-squares)
 }
