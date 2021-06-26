354cff74cd4b42e7d86d2d09c3d8d4a38542e274
StevePro7 <steveprozune@hotmail.com>
2021-06-26 12:09:31 +0100

    no message

--- ch8/crawl3/main.go
+++ ch8/crawl3/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"learning/links"
+)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
 
 func main() {
-	fmt.Println("Hello there")
+
+	worklist := make(chan []string)  // list of URLs may have duplicate
+	unseenLinks := make(chan string) // de-duplicated URLs
+
+	// Add command line arguments to worklist
+	go func() {
+		worklist <- os.Args[1:]
+	}()
+
+	// Create 20 crawler goroutines to fetch each unseen link
+	for i := 0; i < 20; i++ {
+		go func() {
+			for link := range unseenLinks {
+				foundLinks := crawl(link)
+				go func() {
+					worklist <- foundLinks
+				}()
+			}
+		}()
+	}
+
+	// The main goroutine de-duplicates worklist items
+	// and sends the unseen ones to the crawlers
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				unseenLinks <- link
+			}
+		}
+	}
 }
