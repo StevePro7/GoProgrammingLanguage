b90a4a55e9a031540544cf715a1da065e6aaeeb8
StevePro7 <steveprozune@hotmail.com>
2021-06-26 11:38:43 +0100

    no message

--- ch8/countdown2/main.go
+++ ch8/countdown2/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func launch() {
+	fmt.Println("Lift off!")
+}
 
 func main() {
-	fmt.Println("Hello there")
+
+	// create abort channel
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a signle byte
+		abort <- struct{}{}
+	}()
+
+	fmt.Println("Commencing countdown.  Press return to abort.")
+
+	select {
+	case <-time.After(10 * time.Second):
+		// Do nothing
+	case <-abort:
+		fmt.Println("Launch aborted")
+		return
+	}
+
+	launch()
 }
