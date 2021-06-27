0c55d6cf2c50a2cbd10db626ebcae3f9076fcf86
StevePro7 <steveprozune@hotmail.com>
2021-06-27 20:40:03 +0100

    no message

---
+++ ch9/memo2/memo/memo.go
@@ -0,0 +1,34 @@
+package memo
+
+import "sync"
+
+// Func is the type of the function to memoize
+type Func func(string) (interface{}, error)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex // guards cache
+	cache map[string]result
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]result)}
+}
+
+// Get is concurrency safe
+func (memo *Memo) Get(key string) (value interface{}, err error) {
+	memo.mu.Lock()
+	defer memo.mu.Unlock()
+
+	res, ok := memo.cache[key]
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.cache[key] = res
+	}
+	return res.value, res.err
+}
