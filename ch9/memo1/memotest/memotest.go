8a3b0fe7839c933e5129f204c7c4d10e69e07a9c
StevePro7 <steveprozune@hotmail.com>
2021-06-27 20:19:33 +0100

    no message

--- ch9/memo1/memotest/memotest.go
+++ ch9/memo1/memotest/memotest.go
@@ -10,8 +10,7 @@ import (
 	"time"
 )
 
-// httpRequestBody
-func httpRequestBody(url string) (interface{}, error) {
+func httpGetBody(url string) (interface{}, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -20,7 +19,7 @@ func httpRequestBody(url string) (interface{}, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-var HTTPGetBody = httpRequestBody
+var HTTPGetBody = httpGetBody
 
 func incomingURLs() <-chan string {
 	ch := make(chan string)
@@ -47,6 +46,7 @@ type M interface {
 }
 
 func Sequential(t *testing.T, m M) {
+
 	for url := range incomingURLs() {
 		start := time.Now()
 		value, err := m.Get(url)
@@ -54,13 +54,13 @@ func Sequential(t *testing.T, m M) {
 			log.Print(err)
 			continue
 		}
-		fmt.Printf("%s %s %d bytes\n",
+		fmt.Printf("%s, %s, %d bytes\n",
 			url, time.Since(start), len(value.([]byte)))
 	}
 }
 
-m := memo.New(httpGetBody)
-func Concurrent(t *testing.T) {
+func Concurrent(t *testing.T, m M) {
+
 	var n sync.WaitGroup
 	for url := range incomingURLs() {
 		n.Add(1)
@@ -72,7 +72,7 @@ func Concurrent(t *testing.T) {
 				log.Print(err)
 				return
 			}
-			fmt.Printf("%s %s %d bytes\n",
+			fmt.Printf("%s, %s, %d bytes\n",
 				url, time.Since(start), len(value.([]byte)))
 		}(url)
 	}
