32c02aa5c262f5ddeb583606d5f9ab8e1cd27bcf
StevePro7 <steveprozune@hotmail.com>
2021-07-03 19:54:16 +0100

    format

--- ch12/format/format/format.go
+++ ch12/format/format/format.go
@@ -1,10 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 332.
-
-// Package format provides an Any function that can format any value.
-//!+
 package format
 
 import (
@@ -12,33 +5,38 @@ import (
 	"strconv"
 )
 
-// Any formats any value as a string.
+// Any formats any value as a string
 func Any(value interface{}) string {
 	return formatAtom(reflect.ValueOf(value))
 }
 
-// formatAtom formats a value without inspecting its internal structure.
+// formatAtom formats a value without inspecting its internal structure
 func formatAtom(v reflect.Value) string {
 	switch v.Kind() {
+
 	case reflect.Invalid:
 		return "invalid"
+
 	case reflect.Int, reflect.Int8, reflect.Int16,
 		reflect.Int32, reflect.Int64:
 		return strconv.FormatInt(v.Int(), 10)
+
 	case reflect.Uint, reflect.Uint8, reflect.Uint16,
 		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 		return strconv.FormatUint(v.Uint(), 10)
+
 	// ...floating-point and complex cases omitted for brevity...
 	case reflect.Bool:
 		return strconv.FormatBool(v.Bool())
+
 	case reflect.String:
 		return strconv.Quote(v.String())
+
 	case reflect.Chan, reflect.Func, reflect.Ptr, reflect.Slice, reflect.Map:
 		return v.Type().String() + " 0x" +
 			strconv.FormatUint(uint64(v.Pointer()), 16)
+
 	default: // reflect.Array, reflect.Struct, reflect.Interface
 		return v.Type().String() + " value"
 	}
 }
-
-//!-
